ccb8860bebc6ff72bc421a21add9246c9844304f
zhangcs <zhangchengsheng@xinxinapp.cn>
2020-11-19 21:12:28 +0800

    http2 serevr push

---
+++ sites/localhost/h2.php
@@ -0,0 +1,21 @@
+<?php
+header('Link: </static/css/bootstrap.min.css>; rel=preload; as=css, </static/js/jquery.min.js>; rel=preload; as=js, </static/js/bootstrap.min.js>; rel=preload; as=js');
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HTTP/2</title>
+
+    <link href="static/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <h5>你好，HTTP/2</h5>
+
+    <script src="static/js/jquery.min.js"></script>
+    <script src="static/js/bootstrap.min.js"></script>
+  </body>
+</html>
