6cec2b6dd008b0c9a3d0f95fc771f00cf14925fc
zhangcs <zhangchengsheng@xinxinapp.cn>
2020-12-02 19:51:52 +0800

    apollo es kibana

--- sites/localhost/h2.php
+++ sites/localhost/h2.php
@@ -1,4 +1,12 @@
 <?php
+$hasShipTime = 0;
+for ($i=0; $i < 10; $i++) { 
+  if (!$hasShipTime) {
+    $hasShipTime = 1;
+    echo $i . '-' . 111;
+  }
+}
+
 header('Link: </static/css/bootstrap.min.css>; rel=preload; as=css, </static/js/jquery.min.js>; rel=preload; as=js, </static/js/bootstrap.min.js>; rel=preload; as=js');
 ?>
 <!DOCTYPE html>
